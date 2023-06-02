fd38054e372ff5b578337c5cd7676d7251dd45a6
Terry <shiyuntian@126.com>
2023-06-02 12:08:19 -0400

    add search

--- src/components/ApplicationTable.jsx
+++ src/components/ApplicationTable.jsx
@@ -1,10 +1,20 @@
-import { Table, Input, Popconfirm, Form, Typography } from 'antd';
+import {
+  Table,
+  Input,
+  Popconfirm,
+  Form,
+  Typography,
+  Space,
+  Button,
+} from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { useFirestore, useUser } from 'reactfire';
 import { Select } from 'antd';
 import dayjs from 'dayjs';
 import Link from 'antd/es/typography/Link';
+import Highlighter from 'react-highlight-words';
 const EditableCell = ({
   editing,
   dataIndex,
@@ -54,6 +64,79 @@ const ApplicationTable = () => {
   const [form] = Form.useForm();
   const [data, setData] = useState([]);
   const [editingKey, setEditingKey] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : '',
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const isEditing = (record) => record.key === editingKey;
 
@@ -107,6 +190,7 @@ const ApplicationTable = () => {
       dataIndex: 'companyName',
       key: 'companyName',
       width: '15%',
+      ...getColumnSearchProps('companyName'),
       editable: true,
     },
     {
